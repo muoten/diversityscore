eb48e1b0f56289cdde46afa5964e3929cb2581ae
beeva-enriqueotero <enrique.otero@beeva.com>
2018-12-16 18:44:55 +0100

    Final commit

--- gender_detector.py
+++ gender_detector.py
@@ -1,4 +1,7 @@
 import cognitive_face as CF
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+from matplotlib import patches
 
 class GenderDetector():
 
@@ -10,20 +13,50 @@ class GenderDetector():
         CF.BaseUrl.set(BASE_URL)
         self.CF = CF
 
+
+    def detect_faces_from_binary(self, data):
+        faces = self.CF.face.detect(data, attributes='gender')
+        return faces
+
     def detect_gender_from_binary(self, data):
 
         faces = self.CF.face.detect(data, attributes='gender')
-        return faces
+        print(faces)
+
+        females = 0
+        for face in faces:
+            fa = face["faceAttributes"]
+            if fa["gender"] == "female":
+                females = females + 1
+
+        result = {}
+        result["female"] = females
+        result["total"] = len(faces)
+        return result
 
     def detect_gender(self, URI):
         faces = self.CF.face.detect(URI, attributes='gender')
         females = 0
         for face in faces:
             fa = face["faceAttributes"]
-            if fa["gender"] == "Female":
+            if fa["gender"] == "female":
                 females = females+1
 
         result = {}
         result["female"] = females
         result["total"] = len(faces)
         return result
+
+    def plot_gender(self, data):
+        faces = self.CF.face.detect(data, attributes='gender')
+        plt.figure(figsize=(8, 8))
+        ax = plt.imshow(data, alpha=0.6)
+        for face in faces:
+            fr = face["faceRectangle"]
+            fa = face["faceAttributes"]
+            origin = (fr["left"], fr["top"])
+            p = patches.Rectangle(
+                origin, fr["width"], fr["height"], fill=False, linewidth=2, color='b')
+            ax.axes.add_patch(p)
+            plt.text(origin[0], origin[1], "%s, %d" % (fa["gender"].capitalize(), fa["age"]),
+                     fontsize=20, weight="bold", va="bottom")
--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, send_file, make_response
 from flask import jsonify
 from gender_detector import GenderDetector
 app = Flask(__name__)
 import os
 from PIL import Image
 from io import BytesIO
+
+import random
+from io import StringIO
+
+from flask import Flask, make_response, request, flash, redirect
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+from matplotlib import patches
 
 @app.route("/")
 def hello():
@@ -30,7 +39,148 @@ def gender2():
 
     return json
 
+@app.route('/plot.png')
+def plot():
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+
+    xs = range(100)
+    ys = [random.randint(1, 50) for x in xs]
+
+    axis.plot(xs, ys)
+    canvas = FigureCanvas(fig)
+    output = StringIO()
+    canvas.print_png(output)
+    response = make_response(output.getvalue())
+    response.mimetype = 'image/png'
+    return response
+
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+
+    print(file.filename)
+    FILE_IN = "/tmp/myimage.png"
+    file.save(FILE_IN)
+
+    return send_file(FILE_IN, mimetype='image/png')
+
+
+@app.route('/upload_tmp', methods=['GET', 'POST'])
+def upload_tmp():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+
+    print(file.filename)
+    FILE_IN = "/tmp/myimage"
+    file.save(FILE_IN)
+
+
+    #imagefile = file.filename
+
+    #URI = r'/home/milhouse/projects/diversityscore/img/startmeapp2.jpg'
+    URI = FILE_IN
+
+    image_data = open(URI, "rb")
+
+    #image_data = request.data
+    #image_data = file
+    faces = detector.detect_faces_from_binary(image_data)
+    print(faces)
+
+    #image = Image.open(FILE_IN)
+    #image = Image.open(URI)
+    #image = image_data
+    plt.figure(figsize=(8, 8))
+    #ax = plt.imshow(image, alpha=0.6)
+
+    image = plt.imread(FILE_IN)
+    ax = plt.subplots()
+    #ax.imshow(image)
+    for face in faces:
+        fr = face["faceRectangle"]
+        fa = face["faceAttributes"]
+        origin = (fr["left"], fr["top"])
+        p = patches.Rectangle(
+            origin, fr["width"], fr["height"], fill=False, linewidth=2, color='b')
+        ax.add_patch(p)
+        plt.text(origin[0], origin[1], "%s" % (fa["gender"].capitalize()),
+                 fontsize=10, weight="bold", va="bottom")
+    # plt.axis("off")
+
+    FILE_OUT = "/tmp/myout"
+
+    plt.savefig(FILE_OUT)
+
+    return send_file(FILE_OUT, mimetype='image/png')
+
+
+@app.route('/upload2', methods=['GET', 'POST'])
+def upload2():
+    print(request.files)
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+
+    print(file.filename)
+    FILE_IN = "/tmp/myimage.png"
+    file.save(FILE_IN)
+
+    image_data = open(FILE_IN, "rb")
+
+    # image_data = request.data
+    # image_data = file
+    faces = detector.detect_gender_from_binary(image_data)
+    print(faces)
+
+    #headers = {'content-type': 'text/plain'}
+    #response = make_response(faces, 200)
+    #response.headers = headers
+
+    percent = int(faces["female"])*1.0/int(faces["total"])
+    print(percent)
+    percent = percent * 100
+    percent = int(percent)
+    print(percent)
+    mytext = "{}".format(percent)
+    return mytext
+    #return jsonify(faces)
+
+
 if __name__ == "__main__":
     MY_KEY = os.environ["AZURE_FACE_API_KEY"]
     detector = GenderDetector(MY_KEY)
+    app.secret_key = 'super secret key'
+    #app.config['SESSION_TYPE'] = 'filesystem'
+
+    #sess.init_app(app)
     app.run(host='0.0.0.0', port=5000)
